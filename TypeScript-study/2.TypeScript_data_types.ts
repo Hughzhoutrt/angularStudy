05511e0ec1b0e5fba399d0694aeec1d713ba5558
YUHUI ZHOU <hughzhoutrt@gmail.com>
2021-12-19 23:51:13 -0500

    typescript data type

---
+++ TypeScript-study/2.TypeScript_data_types.ts
@@ -0,0 +1,24 @@
+//String, number, boolean, null, undefined, enum, symbol
+
+var str:string = 'hello';
+var num:number = 1;
+var bol:boolean = true;
+var un:undefined = undefined;
+var nul:null = null;
+
+str = null;
+str = undefined;
+
+var callBack = function():void{
+
+}
+
+var num2:any = 1;
+num2 = true;
+num2 = '3';
+
+var b = 1;
+
+var c;
+c = 1;
+c = 'abc';
